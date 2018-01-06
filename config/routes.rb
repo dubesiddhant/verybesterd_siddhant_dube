c006e87d20313b55067a1aa57ca8ffcc497d9f47
Raghu Betina <raghu@firstdraft.com>
2018-01-06 07:51:33 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "dish_lists#index"
   # Routes for the Bookmarked_venue resource:
   # CREATE
